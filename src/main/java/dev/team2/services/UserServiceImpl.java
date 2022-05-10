3e1f5c096b3d77171537fbeda696b2d03456c1ab
prwamasirabo1 <patrick.rwamasirabo@revature.net>
2022-05-10 17:59:24 -0400

    Users initial commit

--- src/main/java/dev/team2/services/UserServiceImpl.java
+++ src/main/java/dev/team2/services/UserServiceImpl.java
@@ -1,4 +1,40 @@
 package dev.team2.services;
 
-public class UserServiceImpl {
-}
+
+
+
+import dev.team2.entities.User;
+import dev.team2.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public User registerUser(User user) {
+        return this.userRepo.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepo.findAll();
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        List <User> users= this.userRepo.findUsersByUsername(username);
+    return ;
+    }
+    }
+
+
+
+
